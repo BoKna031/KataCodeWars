057d5541c859752b7ab27cf1b9d2ca8324d26c4d
Bozidar Arsic <arsicbozidar98@gmail.com>
2022-07-31 15:12:39 +0200

    Parentheses challenge completed

---
+++ KataCodeWars/5_kyu/Parentheses.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCodeWars._5_kyu
+{
+    // challenge https://www.codewars.com/kata/52774a314c2333f0a7000688/train/csharp
+    public class Parentheses
+    {
+        public static bool ValidParentheses(string input)
+        {
+            int counter = 0;
+            foreach (char c in input)
+            {
+                if (c == '(')
+                { 
+                    counter++;
+                }else if(c == ')')
+                {
+                    counter--;
+                }
+
+                if (counter < 0)
+                    return false;
+            }
+            return counter == 0;
+        }
+    }
+}
