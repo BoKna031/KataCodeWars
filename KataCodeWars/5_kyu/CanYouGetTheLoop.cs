2569954cb895ac0357ee7adf9f70b9987ff696bc
Bozidar Arsic <arsicbozidar98@gmail.com>
2022-07-31 16:12:27 +0200

    Can you get the loop challenge completted

---
+++ KataCodeWars/5_kyu/CanYouGetTheLoop.cs
@@ -0,0 +1,18 @@
+﻿namespace KataCodeWars._5_kyu
+{
+    // challenge https://www.codewars.com/kata/52a89c2ea8ddc5547a000863/train/csharp
+    public class CanYouGetTheLoop
+    {
+        /*public static int getLoopSize(LoopDetector.Node startNode)
+        {
+            var dict = new Dictionary<LoopDetector.Node, int>();
+            int index = 0;
+            while (!dict.ContainsKey(startNode))
+            {
+                dict[startNode] = index++;
+                startNode = startNode.next;
+            }
+            return dict.Count - dict[startNode];
+        }*/
+    }
+}
